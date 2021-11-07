249477288037c6eaf57880a5a9c94ffa5d0e5f87
Yu4iy <yurimohyliuk@gmail.com>
2021-11-07 21:45:48 +0100

    add function

--- main.js
+++ main.js
@@ -69,46 +69,50 @@ const posts =
 
 const container = document.getElementById('container')
 
+get (container)
 
-for (let i = 0; i < posts.length; i++){
-let postData =  posts[i]
-	
-container.innerHTML += 
-`
-<div class="post">
-	<div class="post__header">
-		<div class="post-meta">                    
-			<div class="post-meta__icon">
-					<img class="profile-pic" src="${postData.avatar}" alt="Phil Mangione">                    
+
+
+
+function get (container) {
+	for (let i = 0; i < posts.length; i++){
+		let postData =  posts[i]
+			
+		container.innerHTML += 
+		`
+		<div class="post">
+			<div class="post__header">
+				<div class="post-meta">                    
+					<div class="post-meta__icon">
+							<img class="profile-pic" src="${postData.avatar}" alt="Phil Mangione">                    
+					</div>
+					<div class="post-meta__data">
+							<div class="post-meta__author">${postData.userName} ${postData.userSurName}</div>
+							<div class="post-meta__time">${postData.postTime} mesi fa </div>
+					</div>                    
+				</div>
 			</div>
-			<div class="post-meta__data">
-					<div class="post-meta__author">${postData.userName} ${postData.userSurName}</div>
-					<div class="post-meta__time">${postData.postTime} mesi fa </div>
-			</div>                    
+			<div class="post__text">${postData.postText}</div>
+			<div class="post__image">
+				<img src="${postData.postImage}" alt="">
+			</div>
+			<div class="post__footer">
+				<div class="likes js-likes">
+					<div class="likes__cta">
+							<a class="like-button  js-like-button" href="#" data-postid="1">
+								<i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+								<span class="like-button__label">Mi Piace</span>
+							</a>
+					<button class="btn"></button>
+		
+					</div>
+					<div class="likes__counter">
+							Piace a <b id="like-counter-1" class="js-likes-counter">${postData.like}</b> persone
+					</div>
+				</div> 
+			</div>            
 		</div>
-	</div>
-	<div class="post__text">${postData.postText}</div>
-	<div class="post__image">
-		<img src="${postData.postImage}" alt="">
-	</div>
-	<div class="post__footer">
-		<div class="likes js-likes">
-			<div class="likes__cta">
-					<a class="like-button  js-like-button" href="#" data-postid="1">
-						<i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-						<span class="like-button__label">Mi Piace</span>
-					</a>
-			<button class="btn"></button>
-
-			</div>
-			<div class="likes__counter">
-					Piace a <b id="like-counter-1" class="js-likes-counter">${postData.like}</b> persone
-			</div>
-		</div> 
-	</div>            
-</div>
-`
-
+		`
+		
+		}
 }
-
-
